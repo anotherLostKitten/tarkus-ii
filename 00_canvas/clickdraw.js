4b3a84a6dea5e22bb90f98c9725610278bd66cab
Theodore Peters <tpeters@stuy.edu>
2019-01-31 00:37:06 -0500

    ok so basically

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -5,15 +5,25 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     const tgl = document.getElementById("toggle");
     tgl.addEventListener("click",()=>{
-	drawRect = !drawRect;
-	tgl.innerHTML = drawRect?"now drawing rectangle":"now drawing circle";
+		drawRect = !drawRect;
+		tgl.innerHTML = drawRect?"now drawing rectangle":"now drawing circle";
     });
 
     document.getElementById("clear").addEventListener("click",()=>{
-	ctx.clearRect(0,0,c.width,c.height);
+		ctx.clearRect(0,0,c.width,c.height);
     }); 
 
-    c.addEventListener("click",()=>{
-	
+    c.addEventListener("click",(e)=>{
+		console.log(e);
+		console.log(c.getBoundingClientRect());
+		var x = e.clientX - c.getBoundingClientRect().x;
+		var y = e.clientY - c.getBoundingClientRect().y;
+		if(drawRect)
+			ctx.fillRect(x-10,y-10,20,20);
+		else{
+			ctx.beginPath();
+			ctx.ellipse(x,y,10,10,0,0,2*Math.PI);
+			ctx.fill();
+		}
     });
 });
