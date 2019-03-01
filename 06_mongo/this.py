4ac749ca58b326bb28f068eec73e2ce03510b5bd
anotherLostKitten <tpeters@stuy.edu>
2019-02-28 23:36:53 -0500

    ssh

---
+++ 06_mongo/this.py
@@ -0,0 +1,8 @@
+# Team -- Theodore Peters & Ray Onishi
+# Sotdev pd7
+# K06 -- mongo to the polls
+# 2019-02-28
+
+def find(burrow=None,zipc=None,grade=None,thresh=None,
+if __name__=="__main__":
+    borough("Bronx")
