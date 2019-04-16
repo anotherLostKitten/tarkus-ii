7f181ff0e3191e0cbd9c3cd70f2aa855128ed8c0
Theodore Peters <tpeters@stuy.edu>
2019-04-16 06:45:30 -0400

    1 line ezpz

---
+++ 18_listcomp/this.py
@@ -0,0 +1,6 @@
+
+from math import sqrt
+qs=lambda l:qs([i for i in l if i<l[0]])+[i for i in l if i==l[0]]+qs([i for i in l if i>l[0]])if len(l)>1 else l
+print(qs([4,2,3,6,1,2,6,7,8,1,2,3,8,9]))
+ptip=lambda mx:[(i,j,int(sqrt(i*i+j*j)))for i in range(1,mx)for j in range(1,mx)if sqrt(i*i+j*j)%1==0]
+print(ptip(50))
